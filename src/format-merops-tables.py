ca8922370cab484e511c115a908a8d1c2e12f1fe
e-morrison <eliot.morrison@fu-berlin.de>
2023-07-27 22:49:51 +0200

    Specific MEROPS tables

---
+++ src/format-merops-tables.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+import vaex
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import re
+import os
+from pathlib import Path
+import chardet
+
+
+def open_file(sql_file):
+    sql_file = f'data/merops/{sql_file}'
+    with open(sql_file, 'rb') as file:
+        raw_data = file.read()
+        detected_encoding = chardet.detect(raw_data)['encoding']
+
+    with open(sql_file, 'r', encoding=detected_encoding) as file:
+        return file.read()
+
+def save_parquet(list_in, file_name_out):
+    '''
+    Takes a list and saves it as a parquet file. 
+    '''
+    df = pd.DataFrame(list_in)
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, file_name_out)
+
+
+
+def extract_values(insert_statements):
+    '''
+    Take INSERT INTO statements and pull the data into an empty list.
+    '''
+
+    # Initialize empty list
+    data = []
+
+    # Parse each INSERT INTO statement and extract the values
+    for statement in insert_statements:
+        values = re.findall(r'\((.*?)\)', statement)
+        data.extend([v.strip("'") for v in value.split(',')] for value in values)
+    return data
+
+
+def extract_column_headers(create_table_statements):
+    '''
+    This will extract the column headers from the .sql file.
+    '''
+
+    # Initialize empty list
+    column_headers = []
+
+    # Parse each CREATE TABLE statement and extract the values
+    for statement in create_table_statements:
+        values = re.findall(r'`([^`]+)`', statement)
+        column_headers.extend(values)
+
+    return column_headers
+
+
+def gen_file_out_name(sql_file):
+    file_path = Path(sql_file)
+    new_path = Path(file_path.name)
+    return 'data/merops_parsed/' + str(new_path.with_suffix('.parquet'))
+
+
+
+
+def parse_merops_sql(sql_file):
+    '''
+    This function parses an sql file from MEROPS and converts
+    it to a pandas dataframe.
+    '''
+
+    # Open the file 
+    sql_script = open_file(sql_file)
+
+    # Extract all INSERT INTO statements from .sql file
+    insert_statements = re.findall(r'INSERT INTO.*?;', sql_script, re.DOTALL)
+    create_table_statements = re.findall(r'CREATE TABLE.*?;', sql_script, re.DOTALL)
+
+    # Extract values into a data list
+    data = extract_values(insert_statements)
+
+    # Extract column headers
+    column_headers = extract_column_headers(create_table_statements)
+
+    return data, column_headers
+
+
+if __name__ == "__main__":
+    #sql_files = [file1 for file1 in os.listdir('data/merops/') if file1.endswith('.sql')]
+    sql_files = [
+                    'Substrate_search.sql',
+                    'residue_codes.sql',
+                    ]
+
+    for sql_file in sql_files:
+        data, column_headers = parse_merops_sql(sql_file)
+
+        # Check if MEROPS parsed path exists and, if not, create it
+        os.makedirs('data/merops_parsed', exist_ok=True)
+
+        # Generate new file name for parsed file
+        file_name_out = gen_file_out_name(sql_file)
+
+        # Save parsed data as a .parquet file
+        data.insert(0, column_headers)
+        save_parquet(data, file_name_out)
+        print(f'Saved {file_name_out}')
+
+
--- src/parse-merops-tables.py
+++ src/parse-merops-tables.py
@@ -1,105 +1,21 @@
 #!/usr/bin/python
 
 import vaex
-import pandas as pd
-import pyarrow as pa
-import pyarrow.parquet as pq
-import re
-import os
-from pathlib import Path
+import os, sys, re, csv
 
 
-def open_file(sql_file):
-    with open(f'data/merops/{sql_file}', 'r') as file:
-        return file.read()
-
-def save_parquet(list_in, file_name_out):
-    '''
-    Takes a list and saves it as a parquet file. 
-    '''
-    df = pd.DataFrame(list_in)
-    table = pa.Table.from_pandas(df)
-    pq.write_table(table, file_name_out)
-
-
-
-def extract_values(insert_statements):
-    '''
-    Take INSERT INTO statements and pull the data into an empty list.
-    '''
-
-    # Initialize empty list
-    data = []
-
-    # Parse each INSERT INTO statement and extract the values
-    for statement in insert_statements:
-        values = re.findall(r'\((.*?)\)', statement)
-        data.extend([v.strip("'") for v in value.split(',')] for value in values)
-    return data
-
-
-def extract_column_headers(create_table_statements):
-    '''
-    This will extract the column headers from the .sql file.
-    '''
-
-    # Initialize empty list
-    column_headers = []
-
-    # Parse each CREATE TABLE statement and extract the values
-    for statement in create_table_statements:
-        values = re.findall(r'`([^`]+)`', statement)
-        column_headers.extend(values)
-
-    return column_headers
-
-
-def gen_file_out_name(sql_file):
-    file_path = Path(sql_file)
-    new_path = Path(file_path.name)
-    return 'data/merops_parsed/' + str(new_path.with_suffix('.parquet'))
-
-
-
-
-def parse_merops_sql(sql_file):
-    '''
-    This function parses an sql file from MEROPS and converts
-    it to a pandas dataframe.
-    '''
-
-    # Open the file 
-    sql_script = open_file(sql_file)
-
-    # Extract all INSERT INTO statements from .sql file
-    insert_statements = re.findall(r'INSERT INTO.*?;', sql_script, re.DOTALL)
-    create_table_statements = re.findall(r'CREATE TABLE.*?;', sql_script, re.DOTALL)
-
-    # Extract values into a data list
-    data = extract_values(insert_statements)
-
-    # Extract column headers
-    column_headers = extract_column_headers(create_table_statements)
-
-    return data, column_headers
 
 
 if __name__ == "__main__":
-    sql_files = [
-                'cleavage.sql',
-                ]
+    #parquet_files = [file1 for file1 in os.listdir('data/merops_parsed/') if file1.endswith('.parquet')]
 
-    for sql_file in sql_files:
-        data, column_headers = parse_merops_sql(sql_file)
+    parquet_files = [
+                    'Substrate_search.parquet',
+                    'residue_codes.parquet',
+                    ]
 
-        # Check if MEROPS parsed path exists and, if not, create it
-        os.makedirs('data/merops_parsed', exist_ok=True)
-
-        # Generate new file name for parsed file
-        file_name_out = gen_file_out_name(sql_file)
-
-        # Save parsed data as a .parquet file
-        data.insert(0, column_headers)
-        print(file_name_out)
-        save_parquet(data, file_name_out)
-
+    for parquet_file in parquet_files:
+        df = vaex.open(f'data/merops_parsed/{parquet_file}')
+        print('')
+        print(parquet_file)
+        print(df)
